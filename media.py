83fc0052d026360027810612fc5874482e75c203
daniel2234 <daniel2234@me.com>
2017-11-04 14:13:30 -0400

    edited code to follow the PEP-8 Guidelines

--- media.py
+++ media.py
@@ -1,12 +1,23 @@
 import webbrowser
 
-#Created a Movie class to supply the movie data
-class Movie():
-  def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-    self.title = movie_title
-    self.storyline = movie_storyline
-    self.poster_image_url = poster_image
-    self.trailer_youtube_url = trailer_youtube
 
-  def play_trailer(self):
-    webbrowser.open(self.trailer_youtube_url)
+# Created a Movie class to supply the movie data
+class Movie():
+    # Constructor method that applies movie title,
+    # movie storyline, poster image and Youtube Trailer
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 poster_image,
+                 trailer_youtube):
+        # Every instance called has a parameter of movie title,
+        # movie storyline, poster image, and youtube trailer
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        # Everytime play_trailer is called,
+        # this method helps the the Trailer modal display the trailer
+
+    def play_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
